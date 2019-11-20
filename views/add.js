af4496caa794a1e6f1aa8f56049b3942b129c6eb
LucasZaia <lucas.zaia30@gmail.com>
2019-11-20 21:03:11 -0200

    att var

--- views/add.js
+++ views/add.js
@@ -2,6 +2,8 @@ const mysql = require('mysql');
 const con = require('../config/conection');
 const cons = mysql.createConnection(con);
 
-var cmdsq = 'INSERT INTO testes(nome) VALUES ("jao")';
+var i = "caio";
+var cmdsq = "INSERT INTO testes(nome) VALUES ('"+i+"')";
+
 
 cons.query(cmdsq);
--- views/list.js
+++ views/list.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 const con = require('../config/conection');
 const cons = mysql.createConnection(con);
-var id = 1;
-const sql = 'SELECT * FROM testes';
+var i = "caio";
+const sql = 'SELECT * FROM testes WHERE nome = "'+i+'"';
 cons.query(sql, (error, results, fields) => {
   if (error) {
     return console.error(error.message);
